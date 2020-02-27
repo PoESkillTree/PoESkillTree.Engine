087037dab670f206ad9e72f4fd7639532ef97721
brather1ng <7596346+brather1ng@users.noreply.github.com>
2020-02-27 20:23:25 +0100

    Calculate ModifierSourceEntity.IsRare/Unique based on entity
    (only Enemy can be Rare or Unique for simplicity's sake)

--- PoESkillTree.Engine.Computation.Builders/Entities/ModifierSourceEntityWithRarityBuilder.cs
+++ PoESkillTree.Engine.Computation.Builders/Entities/ModifierSourceEntityWithRarityBuilder.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using PoESkillTree.Engine.Computation.Builders.Conditions;
 using PoESkillTree.Engine.Computation.Builders.Stats;
 using PoESkillTree.Engine.Computation.Common;
+using PoESkillTree.Engine.Computation.Common.Builders;
 using PoESkillTree.Engine.Computation.Common.Builders.Conditions;
 using PoESkillTree.Engine.Computation.Common.Builders.Entities;
 using PoESkillTree.Engine.GameModel;
@@ -19,16 +22,17 @@ namespace PoESkillTree.Engine.Computation.Builders.Entities
 
         public IReadOnlyCollection<Entity> Build(Entity modifierSourceEntity) => new[] {modifierSourceEntity};
 
-        public IConditionBuilder IsRare =>
-            new ValueConditionBuilder(ps =>
-                StatBuilderUtils.FromIdentity(_statFactory, "IsRare", typeof(bool), ExplicitRegistrationTypes.UserSpecifiedValue(false))
-                    .Value.Build(ps));
-
-        public IConditionBuilder IsUnique =>
-            new ValueConditionBuilder(ps =>
-                StatBuilderUtils.FromIdentity(_statFactory, "IsUnique", typeof(bool), ExplicitRegistrationTypes.UserSpecifiedValue(false))
-                    .Value.Build(ps));
-
         public IConditionBuilder IsRareOrUnique => IsRare.Or(IsUnique);
+
+        public IConditionBuilder IsRare => new ValueConditionBuilder(ps => BuildFalseExceptForEnemyValue(ps));
+
+        public IConditionBuilder IsUnique => new ValueConditionBuilder(ps => BuildFalseExceptForEnemyValue(ps));
+
+        private IValue BuildFalseExceptForEnemyValue(BuildParameters ps, [CallerMemberName] string identity = "") =>
+            ps.ModifierSourceEntity == Entity.Enemy ? CreateValue(ps, identity) : new Constant(false);
+
+        private IValue CreateValue(BuildParameters ps, string identity) =>
+            StatBuilderUtils.FromIdentity(_statFactory, identity, typeof(bool), ExplicitRegistrationTypes.UserSpecifiedValue(false))
+                .Value.Build(ps);
     }
 }
