bbda0ec2af216ea921ed2414e1a25070a969b047
Emmitt <1337ERJ1337@gmail.com>
2021-11-05 13:14:52 -0700

    Fixed an issue where skill sprite coords where output with "width" and "height" instead of "w" and "h" (#14)

---
+++ PoESkillTree.Engine.GameModel/PassiveTree/Converters/PassiveTreeFlavourTextBoundsConverter.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Drawing;
+using System.Linq;
+
+namespace PoESkillTree.Engine.GameModel.PassiveTree.Converters
+{
+    public class PassiveTreeFlavourTextBoundsConverter : PassiveTreeRectangleFConverter
+    {
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            var jObject = new JObject();
+            if (value is RectangleF rectangle)
+            {
+                jObject.Add("x", rectangle.X);
+                jObject.Add("y", rectangle.Y);
+                jObject.Add("width", rectangle.Width);
+                jObject.Add("height", rectangle.Height);
+            }
+
+            jObject.WriteTo(writer);
+        }
+    }
+}
---
+++ PoESkillTree.Engine.GameModel/PassiveTree/Converters/PassiveTreeSkillSpriteCoordsConverter.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Drawing;
+using System.Linq;
+
+namespace PoESkillTree.Engine.GameModel.PassiveTree.Converters
+{
+    public class PassiveTreeSkillSpriteCoordsConverter : PassiveTreeRectangleFConverter
+    {
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            var jObject = new JObject();
+            if (value is RectangleF rectangle)
+            {
+                jObject.Add("x", rectangle.X);
+                jObject.Add("y", rectangle.Y);
+                jObject.Add("w", rectangle.Width);
+                jObject.Add("h", rectangle.Height);
+            }
+
+            jObject.WriteTo(writer);
+        }
+    }
+}
